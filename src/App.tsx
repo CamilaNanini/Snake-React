9c6699699a6c9d217a5fa8ba54e032ed333101a9
Camila <camila.nanini.2307@gmail.com>
2024-11-21 11:05:20 -0300

    dejando la libreria de animación

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div className="h-screen w-screen flex flex-col justify-start items-center">
+    <div className="h-screen w-screen flex flex-col justify-start items-center mt-6">
       <div className="text-center">
-        <h1 className="text-7xl font-bold hover:animate-shake">Welcome</h1>
+        <h1 className="text-7xl font-bold animate-bounce">Welcome</h1>
         <h3 >To copia de ElMalditoEmi&apos;s page. :)</h3>
       </div>
     </div>
