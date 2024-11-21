d33435418beff414ab2da438ce0f0680c75765a6
Camila <camila.nanini.2307@gmail.com>
2024-11-21 10:58:18 -0300

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="h-screen w-screen flex flex-col justify-start items-center">
+      <div className="text-center">
+        <h1 className="text-7xl font-bold hover:animate-shake">Welcome</h1>
+        <h3 >To copia de ElMalditoEmi&apos;s page. :)</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
